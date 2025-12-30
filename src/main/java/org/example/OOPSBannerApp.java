00ade9a447a9e0e1d72d5268ffa0ba7ec133b24f
K Seeta Rama Raju <kseetaramaraju.qa@gmail.com>
2025-12-30 16:53:12 +0530

    UC2

--- src/main/java/org/example/OOPSBannerApp.java
+++ src/main/java/org/example/OOPSBannerApp.java
@@ -1,19 +1,67 @@
 package org.example;
 
 /**
- * OOPSBannerApp UC1 - OOPS Banner Display Application
+ * OOPSBannerApp UC2 - Render OOPS as Banner (spaces and *)
  *
- * This Class demostrates a simple Java application that displays the object
- * Oriented Programming System OOPS acronym to the console.
+ * This Class demostrates the creation of ASSCII art banner using Object-Oriented Programming Principles
+ * It extends the basic banner concept by displaying the word "OOPS" in a visual format using asterisks(*)
+ * and spaces to form each letter
+ *
+ *
  *
  * @author seetharamaraju
- * @version 1.0
+ * @version 2.0
  */
-public class OOPSBannerApp
-{
-    public static void main( String[] args )
-    {
-        String oops ="OOPS";
-        System.out.println(oops);
+
+/**
+ *  Extend the User Story 1 to display the message "OOPS" in a banner format
+ *  Using characters formed by spaces and asterisks(*) to create a visual
+ *  effect for each letter in the message.
+ */
+public class OOPSBannerApp {
+    // Main Method to run the banner display
+    public static void main(String[] args) {
+
+        // Define String Array variable to hold the oops banner lines
+        // Each line represents a row in the banner for the letters O,O,P,S
+        String[] O = {
+                " ***** ",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                " ***** "
+        };
+
+        String[] P = {
+                "****** ",
+                "*     *",
+                "*     *",
+                "****** ",
+                "*      ",
+                "*      ",
+                "*      "
+        };
+
+        String[] S = {
+                " ***** ",
+                "*      ",
+                "*      ",
+                " ***** ",
+                "      *",
+                "      *",
+                " ***** "
+        };
+
+        // Banner height is 7
+        for (int i = 0; i < 7; i++) {
+            String line = String.join("   ",
+                    O[i], O[i], P[i], S[i]);
+            System.out.println(line);
+        }
+
     }
+
+
 }
